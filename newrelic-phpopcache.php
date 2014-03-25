1655b81ca27765f3b1c2679b513138234ef9d81e
Steven King <kingrst@gmail.com>
2014-03-24 21:48:44 -0400

    adding config value for platform_api_uri

--- newrelic-phpopcache.php
+++ newrelic-phpopcache.php
@@ -94,7 +94,14 @@
 	  if ( isset($config_values['license_key']) ) {
             $this->license_key = $config_values['license_key'];
 	  } else {
-	    return 20;
+	    return 21;
+	  }
+
+          // If the platform_api_uri is set in the config then set it, else leave it as the default
+          if ( isset($config_values['platform_api_uri']) ) {
+            $this->platform_api_uri = $config_values['platform_api_uri'];
+          } else {
+            return 31;
           }
 
 	  // A config file was found and successfully parsed, return true
